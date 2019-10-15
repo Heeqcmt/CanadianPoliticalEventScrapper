838beab7acba28edbeb91b310a519e99fddae3d1
YuMing Zhang <yumingzhang94@gmail.com>
2019-10-15 13:41:03 -0400

    transformed BCNDP to database insert script

--- BC/NDPBC.py
+++ BC/NDPBC.py
@@ -2,10 +2,23 @@ import requests
 import urllib.request
 import time
 from bs4 import BeautifulSoup
+import json
+import mysql.connector
 #NDP BC
 #need to add data storing 
 
-f = open("NDPBC.txt","w+")
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user = "root",
+    passwd= "XYM88uosjg",
+    database="ndpbc"
+    
+    
+)
+
+mycursor = mydb.cursor()
+
 url = 'https://www.bcndp.ca/events?action_handler=block--campaign-events-search&action=block--campaign-events-search--search-all&json=1'
 
 response = requests.get(url)
@@ -14,14 +27,23 @@ data = response.json()
 
 numOfEvent = len(data["markers"])
 
+eventDic ={}
+
+sqlInsert = "INSERT INTO ndp (title,description,address,province,location,date,party,link) VALUES (%s, %s, %s, %s, %s, %s, %s,%s)"
+
 for i in range(numOfEvent):
-    f.write("Title: "+data["markers"][i]["title"]+"\n")
-    f.write("Date: "+data["markers"][i]["date"]+"\n")
-    f.write("Location: "+data["markers"][i]["location"]+"\n")
-    f.write("Address: "+data["markers"][i]["address"]+"\n")
-    f.write("Postal Code: "+data["markers"][i]["postal_code"]+"\n")
-    f.write("Description: "+data["markers"][i]["description"]+"\n")
-    f.write("Link: "+data["markers"][i]["link"]+"\n\n\n")
-    
+    eventDic["title"]=data["markers"][i]["title"]
+    eventDic["date"]=data["markers"][i]["date"]
+    eventDic["location"]=data["markers"][i]["location"]
+    eventDic["address"]=data["markers"][i]["address"]
+    eventDic["postal Code"]=data["markers"][i]["postal_code"]
+    eventDic["description"]=data["markers"][i]["description"]
+    eventDic["link"]=data["markers"][i]["link"]
+    eventDic["party"]="NDP"
+    eventJson = json.dumps(eventDic)
+    val = (eventDic["title"],eventDic["description"],eventDic["address"],"BC",eventDic["location"], eventDic["date"],"NDP",eventDic["link"])
+    mycursor.execute(sqlInsert,val)
+    mydb.commit()
+    print(mycursor.rowcount, "record inserted.")
 
       
