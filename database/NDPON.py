fda113a9600fa0982a4c6c3a751bd4711d24d1a8
YuMing Zhang <yumingzhang94@gmail.com>
2019-11-28 11:26:53 -0500

    done

--- database/NDPON.py
+++ database/NDPON.py
@@ -2,20 +2,39 @@ import requests
 import urllib.request
 import time
 from bs4 import BeautifulSoup
-#NDP ON
-#need to add data storing 
+import mysql.connector
 
+#NDP MN
+#need to add data storing 
+mydb = mysql.connector.connect(
+    host="mymysql.senecacollege.ca",
+    user = "prj666_193a03",
+    passwd= "adQZ@8552",
+    database="prj666_193a03"   
+)
+mycursor = mydb.cursor()
+eventDic={}
+sqlInsert = "INSERT INTO OFFICIALEVENT (title,description,province,location,date,party,link) VALUES ( %s,%s, %s, %s, %s, %s,%s)"
 
 url = 'https://www.ontariondp.ca/events'
+
+
+response = requests.get(url)
+
+soup = BeautifulSoup(response.text,"html.parser")
+
+
+
 
 response = requests.get(url)
 
 soup = BeautifulSoup(response.text,"html.parser")
 
 eventList = soup.find_all('div','block--event-list--event')
-
+print(eventList)
 for event in eventList:
-    f.write("Date: "+ event.find('div','event-date').text+"\n")
-    f.write("Title: "+ event.find('a').text+"\n")      
-    f.write("Location: "+ event.find('div','event-location').text+"\n") 
-    f.write("RSVP: "+ event.find('a','social-tab rsvp-tab')['href']+"\n")
+    eventDic["date"]=event.find('div','event-date').text
+    eventDic["title"]=event.find('a').text
+    eventDic["location"]= event.find('div','event-location').text
+    eventDic["link"]=event.find('a','social-tab rsvp-tab')['href']
+    print(eventDic)
